b66a14977dee892376188df13e6ea941a5a2e059
Spencer Hilvitz <spencer.hilvitz@gmail.com>
2017-11-17 22:39:45 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -22,7 +22,7 @@ class Question{
     const self = this;
 
     return new Promise(function(resolve) {
-      const sql = 'INSERT INTO questions '
+      const sql = 'INSERT INTO questions (content) VALUES (?)'
     })
   }
 }
